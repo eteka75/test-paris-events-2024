f11a476cdc96c0d6f0d5e98c060b1bfcb4017bc5
ETEKA Wilfried <etekawilfried@gmail.com>
2024-10-20 23:39:38 +0200

    Feat: Add app folder with css code(home,event,layout,notfound)

---
+++ app/event/[id]/page.tsx
@@ -0,0 +1,99 @@
+import { fetchEventById } from "@/lib/fetchEvents";
+import { Calendar1, ChevronLeft, Link2Icon, MapPin } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+const Event = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+
+  const event = await fetchEventById(id);
+
+  if (!event) {
+    notFound();
+  }
+
+  return (
+    <div className="shadow py-8 px-10 border rounded-md">
+      <div className="flex gap-1  -ml-6 font-bold items-start">
+        <Link href={"/"} className="hover:text-blue-600 mt-1">
+          <ChevronLeft /> <span className="sr-only">Retour</span>
+        </Link>
+        <h1 className="text-2xl">{event.fields.title}</h1>
+      </div>
+
+      <div className="text-sm text-gray-500 flex flex-wrap  gap-2 space-x-2  my-3">
+        {event.fields.date_start && (
+          <p className="flex whitespace-pre-wrap  gap-1 items-center">
+            <Calendar1 className="w-4 h-4 -mt-0.5" />{" "}
+            {new Date(event.fields.date_start ?? "").toLocaleDateString()}
+          </p>
+        )}
+        {event.fields.address_city && (
+          <p className="flex whitespace-pre-wrap gap-1 items-center">
+            <MapPin className="w-4 h-4 -mt-0.5" />
+            {event.fields.address_city ?? null}
+            {event.fields.address_name
+              ? ", " + event.fields.address_name
+              : null}
+          </p>
+        )}
+        {event.fields.url && (
+          <a
+            href={event.fields.url}
+            target="_blanck"
+            className="flex underline whitespace-pre-wrap gap-1 items-start"
+          >
+            <Link2Icon className="w-4 h-4 -mt-0.5" />
+            {event.fields.url ?? null}
+          </a>
+        )}
+      </div>
+      <div>
+        {event.fields.cover_url && (
+          <Image
+            src={event.fields.cover_url}
+            alt={event.fields.title}
+            className="w-full h-48 object-cover rounded-lg"
+            width={500}
+            height={500}
+          />
+        )}
+      </div>
+      {event.fields.lead_text && (
+        <div className=" text-lg font-bold py-4">{event.fields.lead_text}</div>
+      )}
+      <div
+        className="text-lg space-y-4"
+        dangerouslySetInnerHTML={{ __html: event.fields.description }}
+      />
+      <div>
+        <Link
+          href={"/"}
+          className=" w-32 mt-4 bg-gray-100 rounded-md shadow-sm flex whitespace-nowrap py-2 px-5 hover:text-white hover:bg-blue-500 "
+        >
+          <ChevronLeft /> Retour
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Event;
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const id = params.id || "";
+  const event = await fetchEventById(id);
+
+  if (!event) {
+    return {
+      title: "Événement non trouvé",
+      description: "Aucun événement trouvé pour cet identifiant.",
+    };
+  }
+
+  return {
+    title: `${event.fields.title} - Beebs Events`,
+    description: event.fields.lead_text || "Description de l'événement.",
+  };
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,26 @@
+// app/not-found/page.tsx
+
+import { Home } from "lucide-react";
+import Link from "next/link";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col  max-w-screen-sm  items-center justify-center min-h-screen text-center">
+      <div className="p-8 rounded-lg hover:bg-gray-50  shadow border">
+        <h1 className="text-6xl font-bold">404</h1>
+        <h2 className="mt-4 text-2xl">Page Not Found</h2>
+        <p className="mt-2 text-lg">
+          Désolé, la page que vous recherchez n&apos;existe pas.
+        </p>
+        <Link
+          href="/"
+          className="mt-6 flex gap-2 justify-center items-center px-6 py-2 bg-blue-500 text-white rounded-full border-0 shadow-sm"
+        >
+          <Home className="h-4 w-4" /> Retour à l&apos;accueil
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
