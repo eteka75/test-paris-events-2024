af1f9da30343bcbad7e90b684166f1b8620f48ab
ETEKA Wilfried <etekawilfried@gmail.com>
2024-10-21 11:02:01 +0200

    Feat:Add next config to stop console logs

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,18 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (process.env.NODE_ENV === "production" && !isServer) {
+      // Désactiver tous les logs côté client en production
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          "console.log": "() => {}",
+          "console.debug": "() => {}",
+        })
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
