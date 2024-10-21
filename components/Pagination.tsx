efef677315adb1ed412594783ce6db4399aacdc1
ETEKA Wilfried <etekawilfried@gmail.com>
2024-10-21 03:56:37 +0200

    Fix: Redirect back && Pagination btn

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -47,7 +47,7 @@ const Pagination: React.FC<PaginationProps> = ({
         {!loading && totalPages > 0 && totalPages >= currentPage && (
           <>
             {" "}
-            <div className="text-center lg:text-start">
+            <div className="text-center mt-2 md:mt-0 lg:text-start">
               {" "}
               Page {currentPage} sur {totalPages}
             </div>
