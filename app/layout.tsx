7f3e6078263203f96b9745053d87d26ab79196e2
ETEKA Wilfried <etekawilfried@gmail.com>
2024-10-21 02:47:37 +0200

    Fix: Edit layout, add view prot

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: " Beebs Event",
-  description: "Découvrez tous évènements sur Paris et environs",
+  title: "Beebs Event",
+  description: "Découvrez tous les événements sur Paris et environs",
 };
 
 export default function RootLayout({
@@ -24,16 +24,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <main className="px-4 py-6 max-w-screen-sm mx-auto grid grid-rows-[50px_1fr_110px] lg:grid-rows-[50px_1fr_60px] min-h-screen">
           <header>
             <Link href="/">
-              {" "}
               <h1 className="uppercase bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 text-transparent bg-clip-text text-xl md:text-2xl xl:text-3xl text-center font-bold">
-                Les évènements à Paris
+                Les événements à Paris
               </h1>
             </Link>
           </header>
@@ -43,17 +47,20 @@ export default function RootLayout({
               Accueil
             </Link>
             <Link className="hover:underline" href={"#"}>
-              A propos
+              À propos
             </Link>
             <Link className="hover:underline" href={"#"}>
               Conditions
             </Link>
             <Link className="hover:underline" href={"#"}>
-              Les évènements sur Paris
+              Les événements sur Paris
             </Link>
             <Link className="hover:underline" href={"#"}>
-              Autres évènements
+              Autres événements
             </Link>
+            <div className="my-2 text-center text-xs text-gray-500">
+              <p>&copy; {new Date().getFullYear()} Beebs Event.</p>
+            </div>
           </footer>
         </main>
       </body>
