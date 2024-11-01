5ee36ee23941ceec2e005f63915ddcc3e231a574
ETEKA Wilfried <etekawilfried@gmail.com>
2024-11-01 05:27:02 +0100

    Feat: Add swipper & edit links

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -100,10 +100,10 @@ export default function Filters({ onSearch, search }: FiltersProps) {
       >
         <SwiperSlide>
           <Link
-            className={`text-xs border bg-slate-100 md:text-sm rounded-full py-1 px-2 flex justify-center items-center ${
+            className={`text-xs   md:text-sm rounded-full py-1 px-2 flex justify-center items-center ${
               !activeCity && !search
                 ? "bg-blue-500 text-white"
-                : "bg-white dark:bg-gray-800"
+                : "bg-gray-100 dark:bg-gray-800"
             }`}
             href={`/?nb_par_page=${nbPerPage}`}
             onClick={() => handleCityClick(null)}
@@ -117,7 +117,7 @@ export default function Filters({ onSearch, search }: FiltersProps) {
               className={`text-xs md:text-sm max-w-40 text-ellipsis truncate whitespace-nowrap rounded-full py-1 px-2 flex justify-center items-center ${
                 activeCity === city
                   ? "bg-blue-500 text-white"
-                  : "bg-white dark:bg-gray-800"
+                  : "bg-gray-100 dark:bg-gray-800"
               }`}
               href={`/?search=${city}&nb_par_page=${nbPerPage}`}
               onClick={() => handleCityClick(city)}
